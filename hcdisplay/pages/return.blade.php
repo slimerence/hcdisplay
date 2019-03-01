1bd5442b3fd585c7966f6ab5f5d53bd728f58c56
slimerence <39113678+slimerence@users.noreply.github.com>
2019-03-01 13:55:09 +1100

    Update return.blade.php

--- hcdisplay/pages/return.blade.php
+++ hcdisplay/pages/return.blade.php
@@ -17,7 +17,9 @@
             <div class="about-item">
                 <h3>Shipping Services</h3>
                 <p>We offer our clients a shipping service to the required destination for the products we produce. Our cargo specialists make careful consideration and propose the optimal means of delivery for your goods, allowing you to minimize your outgoings and receive your product at the right time.</p>
-                <p>Once the product is prepared, it can be shipped in various ways. For example, on a tight deadline the best option is shipping by air, whereas it is more economical to ship by sea. All products undergo quality controls before shipping, and are then packaged and loaded into containers. A company employee must be present at the loading to make sure that the products are loaded properly and to full capacity.</p>
+                <p>Once the product is prepared, it can be shipped in various ways. For example, on a tight deadline the best option is shipping by air, whereas it is more economical to ship by sea. All products undergo quality controls before shipping, and are then packaged and loaded into containers. A company employee must be present at the loading to make sure that the products are loaded properly and to full capacity.<br/>
+                    <strong>Please contact us about the shipping cost before placing the order.</strong></p>
+
                 <p>Pick up: Pick up is from Melbourne(26 Prestige Dr, Clayton South VIC 3169)</p>
 
                 <h3>Payment Term</h3>
